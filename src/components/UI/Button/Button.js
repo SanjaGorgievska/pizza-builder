9fd25e67e17307f1c6375f53b3b9caafca9d5ae0
SanjaGorgievska <57102469+SanjaGorgievska@users.noreply.github.com>
2021-04-21 00:37:54 +0200

    Update Button.js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,8 +3,6 @@ import classes from './Button.css';
 
 const button = (props) => (
     <button
-    //we have to pass the string in the end
-    //imame niza od stringovi 
         className={[classes.Button, classes[props.btnType]].join(' ')}
         onClick={props.clicked}>
             {props.children}
