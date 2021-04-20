d8ec1d6ab211094dbc3523e744d4227fccd7e70a
SanjaGorgievska <57102469+SanjaGorgievska@users.noreply.github.com>
2021-04-21 00:35:16 +0200

    Update Backdrop.js

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import classes from './Backdrop.css';
 
-//ako props e true ke vratime div ako ne ke vratime null
-//onClick={props.clicked} ova go koristime ako klikneme na pozadinata pozadi na backdrop
-//i da se vratime nazad
-
 const backdrop = (props) => (
     props.show ? <div className={classes.Backdrop} onClick={props.clicked}> </div> : null
 );
 
 export default backdrop;
 
-//kade ke go koristime ovoj backdrop. Moze na poveke mesta
-//edno mesto ke bide Modal komponentata
